8ff3bc2614352f2887d23b439dbf613535d44794
Karlo Nenadic <karlo.kac.kkgmail.com>
2024-02-10 13:22:12 +0100

    Fix: quill build

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import dynamic from 'next/dynamic';
-const ReactQuill = dynamic(import('react-quill'), {
-	ssr: false,
-});
-import 'react-quill/dist/quill.snow.css';
 import { StyleProp } from '@datatypes/StyleProps';
 import classNames from 'classnames';
+import 'react-quill/dist/quill.snow.css';
 
 const RichTextEditor = ({ className }: StyleProp) => {
 	const [value, setValue] = useState('');
+	const ReactQuill = useMemo(() => {
+		return dynamic(() => import('react-quill'), {
+			loading: () => <p>loading...</p>,
+			ssr: false,
+		});
+	}, []);
 
 	return <ReactQuill theme="snow" value={value} onChange={setValue} className={classNames(className)} />;
 };
